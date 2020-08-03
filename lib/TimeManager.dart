6a63fcdf0b5cca89362af835c30960c87f4c0e56
Ryan Boyles <ryanboyles14@gmail.com>
2020-08-03 15:15:12 -0700

    Revised modular design to include single scroll list of full timestamps

--- lib/TimeManager.dart
+++ lib/TimeManager.dart
@@ -12,7 +12,7 @@ class _TimeStampState extends State<TimeStamp> {
   String ampm = "";
   String time = ""; //time display string
 
-  List timestamps = new List<int>();
+  List timestamps = new List<String>();
 
   _TimeStampState(){
     sync(); //get timestamp on startup
@@ -24,14 +24,6 @@ class _TimeStampState extends State<TimeStamp> {
       minute = getMinute();
       ampm = getAMPM();
 
-      // timestamp to save is in military format, so only one list needed
-      int t = hour * 100 + minute;
-      // save timestamp to list if not in list
-      if(timestamps.indexOf(t) == -1) {
-        timestamps.add(t);
-        timestamps.sort((a, b) => a.compareTo(b));
-      }
-
       //convert from military time
       if(hour == 0)
         hour = 12;
@@ -40,6 +32,11 @@ class _TimeStampState extends State<TimeStamp> {
 
       // time format: 00:00 XM
       setTime(hour, minute, ampm);
+
+      // save timestamp to list if not in list
+      if(timestamps.indexOf(time) == -1) {
+        timestamps.add(time);
+      }
   }
 
   void updateText(bool isSync){
@@ -115,31 +112,22 @@ class _TimeStampState extends State<TimeStamp> {
   // Launches TimeEdit screen, awaits result from Navigator.pop
   // result is a list containing the hour, minute, and AM/PM
   _navigateEdit(BuildContext context) async {
-    List l = await Navigator.push(
+    int i = await Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (context) => TimestampSelect(hour, minute, ampm, timestamps)
+            builder: (context) => TimestampSelect(timestamps)
         )
     );
 
-    hour = l[0];
-    minute = l[1];
-    ampm = l[2] == 0 ? "AM" : "PM";
-
-    // time format: 00:00 XM
-    setTime(hour, minute, ampm);
-
+    time = timestamps[i];
     updateText(false);
   }
 }
 
 class TimestampSelect extends StatefulWidget {
-  final int hour;
-  final int minute;
-  final String ampm;
-  final List<int> timestamps;
+  final List<String> timestamps;
 
-  const TimestampSelect (this.hour, this.minute, this.ampm, this.timestamps);
+  const TimestampSelect (this.timestamps);
 
   @override
   _TimestampSelectState createState() => _TimestampSelectState();
@@ -147,26 +135,17 @@ class TimestampSelect extends StatefulWidget {
 
 class _TimestampSelectState extends State<TimestampSelect> {
 
+  /* Remnant of modular design
   int _currentHour;
   int _currentMinute;
   int _currentAMPM;
+   */
 
+  int _currentTime;
   //https://inducesmile.com/google-flutter/how-to-change-the-background-color-of-selected-listview-in-flutter/
   // change current index for style change
-  _setTime(String val, int i){
-    setState(() {
-      switch(val){
-        case "Hour":
-          _currentHour = i;
-          break;
-        case "Min":
-          _currentMinute = i;
-          break;
-        case "AMPM":
-          _currentAMPM = i;
-          break;
-      }
-    });
+  _setTime(int i){
+    setState(() => _currentTime = i);
   }
 
   @override
@@ -191,12 +170,18 @@ class _TimestampSelectState extends State<TimestampSelect> {
     // generate lists for hours, minutes, AM/PM
     // style of text is highlighted to the selected time in the scrollwheel
     // if user has not scrolled, the most recent saved timestamp is highlighted
+    List<Widget> times = List.generate(widget.timestamps.length,
+      (index) => Text(widget.timestamps[index],
+        style: (_currentTime == null && index == 0) || _currentTime == index ? _highlight : _style));
+
+    /* Remnant of modular design
     List<Widget> hourList = List<Widget>.generate(12, (index) => Text("${index + 1}",
         style: (_currentHour == null && index == widget.hour - 1) || _currentHour == index ? _highlight : _style));
     List<Widget> minuteList = List<Widget>.generate(60, (index) => Text("$index",
         style: (_currentMinute == null && index == widget.minute) || _currentMinute == index ? _highlight : _style));
     List<Widget> ampmList = List<Widget>.generate(2, (index) => Text(index == 0 ? "AM" : "PM",
         style: (_currentAMPM == null && index == (widget.ampm.compareTo("AM") == 0 ? 0 : 1)) ||_currentAMPM == index ? _highlight : _style));
+     */
 
     return Container(
       decoration: new BoxDecoration(
@@ -208,6 +193,20 @@ class _TimestampSelectState extends State<TimestampSelect> {
         children:[
           Container(
             decoration: _scrollList,
+            alignment: Alignment.center,
+            height:300,
+            child: ListWheelScrollView(
+              // set initial value to last timestamp value
+              controller: FixedExtentScrollController(initialItem: widget.timestamps.length - 1),
+              itemExtent: _style.fontSize,
+              children: times,
+              squeeze: 0.5,
+              onSelectedItemChanged: (i) => {
+                _setTime(i)
+              },
+            )
+
+            /* Remnant of modular design
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -260,12 +259,14 @@ class _TimestampSelectState extends State<TimestampSelect> {
                   )
                 ),
             ]),
+             */
           ),
 
           //Pass the selected time back to TimeStamp
           RaisedButton(
               child: Text("Save"),
               onPressed: (){
+                /* Remnant of modular design
                 // if user did not scroll these value, set them to the last updated timestamp values
                 if(_currentHour == null)
                   _currentHour = widget.hour;
@@ -273,8 +274,11 @@ class _TimestampSelectState extends State<TimestampSelect> {
                   _currentMinute = widget.minute;
                 if(_currentAMPM == null)
                   _currentAMPM = widget.ampm.compareTo("AM") == 0 ? 0 : 1;
+
                 List l = [_currentHour, _currentMinute, _currentAMPM];
-                Navigator.pop(context, l);
+                 */
+
+                Navigator.pop(context, _currentTime);
               }
           )
         ],

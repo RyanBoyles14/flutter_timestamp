6f8976ffd8663b0f6ae3fa15f2cbd4a2ccb94055
Ryan Boyles <ryanboyles14@gmail.com>
2020-08-03 13:54:58 -0700

    Improved list indexing and starting values, added styling

--- lib/TimeManager.dart
+++ lib/TimeManager.dart
@@ -12,6 +12,8 @@ class _TimeStampState extends State<TimeStamp> {
   String ampm = "";
   String time = ""; //time display string
 
+  List timestamps = new List<int>();
+
   _TimeStampState(){
     sync(); //get timestamp on startup
   }
@@ -21,6 +23,14 @@ class _TimeStampState extends State<TimeStamp> {
       hour = getHour();
       minute = getMinute();
       ampm = getAMPM();
+
+      // timestamp to save is in military format, so only one list needed
+      int t = hour * 100 + minute;
+      // save timestamp to list if not in list
+      if(timestamps.indexOf(t) == -1) {
+        timestamps.add(t);
+        timestamps.sort((a, b) => a.compareTo(b));
+      }
 
       //convert from military time
       if(hour == 0)
@@ -73,6 +83,7 @@ class _TimeStampState extends State<TimeStamp> {
         ),
 
         child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text( //Timestamp text
               "$time",
@@ -107,13 +118,13 @@ class _TimeStampState extends State<TimeStamp> {
     List l = await Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (context) => TimestampSelect(hour, minute)
+            builder: (context) => TimestampSelect(hour, minute, ampm, timestamps)
         )
     );
 
     hour = l[0];
     minute = l[1];
-    ampm = l[2];
+    ampm = l[2] == 0 ? "AM" : "PM";
 
     // time format: 00:00 XM
     setTime(hour, minute, ampm);
@@ -125,8 +136,10 @@ class _TimeStampState extends State<TimeStamp> {
 class TimestampSelect extends StatefulWidget {
   final int hour;
   final int minute;
+  final String ampm;
+  final List<int> timestamps;
 
-  const TimestampSelect (this.hour, this.minute);
+  const TimestampSelect (this.hour, this.minute, this.ampm, this.timestamps);
 
   @override
   _TimestampSelectState createState() => _TimestampSelectState();
@@ -134,17 +147,56 @@ class TimestampSelect extends StatefulWidget {
 
 class _TimestampSelectState extends State<TimestampSelect> {
 
+  int _currentHour;
+  int _currentMinute;
+  int _currentAMPM;
+
+  //https://inducesmile.com/google-flutter/how-to-change-the-background-color-of-selected-listview-in-flutter/
+  // change current index for style change
+  _setTime(String val, int i){
+    setState(() {
+      switch(val){
+        case "Hour":
+          _currentHour = i;
+          break;
+        case "Min":
+          _currentMinute = i;
+          break;
+        case "AMPM":
+          _currentAMPM = i;
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    int finalHour = widget.hour;
-    int finalMinute = widget.minute;
-    String ampm = "AM";
-    final _style = Theme.of(context).textTheme.headline3;
+    final _style = TextStyle(
+      color: Colors.white,
+      decoration: TextDecoration.none,
+      fontSize: 36
+    );
 
-    List<Widget> hourList = List<Widget>.generate(12, (index) => Text((index + widget.hour) % 12 == 0 ? "12" : "${(index + widget.hour) % 12} ", style: _style));
-    List<Widget> minuteList = List<Widget>.generate(60, (index) => Text("${(index + widget.minute) % 60}", style: _style));
-    List<Widget> ampmList =  [Text("AM", style: _style), Text("PM",  style: _style)];
+    final _highlight = TextStyle(
+      color: Colors.lightBlue,
+      decoration: TextDecoration.none,
+      fontSize: 36
+    );
+
+    final _scrollList = new BoxDecoration(
+        color: Colors.white54
+    );
+
+    // generate lists for hours, minutes, AM/PM
+    // style of text is highlighted to the selected time in the scrollwheel
+    // if user has not scrolled, the most recent saved timestamp is highlighted
+    List<Widget> hourList = List<Widget>.generate(12, (index) => Text("${index + 1}",
+        style: (_currentHour == null && index == widget.hour - 1) || _currentHour == index ? _highlight : _style));
+    List<Widget> minuteList = List<Widget>.generate(60, (index) => Text("$index",
+        style: (_currentMinute == null && index == widget.minute) || _currentMinute == index ? _highlight : _style));
+    List<Widget> ampmList = List<Widget>.generate(2, (index) => Text(index == 0 ? "AM" : "PM",
+        style: (_currentAMPM == null && index == (widget.ampm.compareTo("AM") == 0 ? 0 : 1)) ||_currentAMPM == index ? _highlight : _style));
 
     return Container(
       decoration: new BoxDecoration(
@@ -152,64 +204,76 @@ class _TimestampSelectState extends State<TimestampSelect> {
       ),
 
       child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children:[
-          Row(
-            children: [
-              ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxHeight: 300,
-                  maxWidth: 100
-                ),
-                child: ListWheelScrollView.useDelegate(
-                  itemExtent: _style.fontSize,
-                  childDelegate: ListWheelChildLoopingListDelegate(
-                    children: hourList,
-                  ),
-                  squeeze: 0.5,
-                  onSelectedItemChanged: (i) => {
-                    finalHour = (widget.hour + i) % 12,
-                    if(finalHour == 0)
-                      finalHour = 12,
-                  },
-                ),
-              ),
-              ConstrainedBox(
-                  constraints: BoxConstraints(
-                      maxHeight: 300,
-                      maxWidth: 100
-                  ),
+          Container(
+            decoration: _scrollList,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 300,
                   child: ListWheelScrollView.useDelegate(
+                    // set initial value to last timestamp value
+                    controller: FixedExtentScrollController(initialItem: widget.hour - 1),
+                    itemExtent: _style.fontSize,
+                    childDelegate: ListWheelChildLoopingListDelegate(
+                      children: hourList,
+                    ),
+                    squeeze: 0.5,
+                    onSelectedItemChanged: (i) => {
+                      _setTime("Hour", i)
+                    },
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 300,
+                  child: ListWheelScrollView.useDelegate(
+                    // set initial value to last timestamp value
+                    controller: FixedExtentScrollController(initialItem: widget.minute),
                     itemExtent: _style.fontSize,
                     childDelegate: ListWheelChildLoopingListDelegate(
                       children: minuteList,
                     ),
                     squeeze: 0.5,
                     onSelectedItemChanged: (i) => {
-                      finalMinute = (widget.minute + i) % 60
+                      _setTime("Min", i)
                     },
-                  )
-              ),
-              ConstrainedBox(
-                  constraints: BoxConstraints(
-                      maxHeight: 300,
-                      maxWidth: 100
                   ),
+                ),
+                Container(
+                  width: 100,
+                  height: 300,
                   child: ListWheelScrollView(
+                    // set initial value to last timestamp value
+                    controller: FixedExtentScrollController(
+                        initialItem: widget.ampm.compareTo("AMP") == 0 ? 0 : 1
+                    ),
                     itemExtent: _style.fontSize,
                     children: ampmList,
                     onSelectedItemChanged: (i) => {
-                      ampm = (i == 0) ? "AM" : "PM"
+                      _setTime("AMPM", i)
                     },
                     squeeze: 0.5
                   )
-              ),
-          ]),
+                ),
+            ]),
+          ),
 
           //Pass the selected time back to TimeStamp
           RaisedButton(
               child: Text("Save"),
               onPressed: (){
-                List l = [finalHour, finalMinute, ampm];
+                // if user did not scroll these value, set them to the last updated timestamp values
+                if(_currentHour == null)
+                  _currentHour = widget.hour;
+                if(_currentMinute == null)
+                  _currentMinute = widget.minute;
+                if(_currentAMPM == null)
+                  _currentAMPM = widget.ampm.compareTo("AM") == 0 ? 0 : 1;
+                List l = [_currentHour, _currentMinute, _currentAMPM];
                 Navigator.pop(context, l);
               }
           )
